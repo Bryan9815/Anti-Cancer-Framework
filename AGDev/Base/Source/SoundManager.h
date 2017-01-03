d638918f3b2ba2d950625fa65e0b097b9a2c08a0
guanhui1998 <lguanhui14@gmail.com>
2017-01-03 22:06:45 +0800

    trying to implement sound manager class but it cant detect irrklangggggggggGGGGGGGGFFFFFFFFFFFFFFFFFFFFFFFFFFUUUUUUu

---
+++ AGDev/Base/Source/SoundManager.h
@@ -0,0 +1,22 @@
+#ifndef SOUND_MANAGER_H
+#define SOUND_MANAGER_H
+
+#include "SingletonTemplate.h"
+//#include <irrKlang.h>
+
+//using namespace irrklang;
+class SoundManager
+{
+public:
+    SoundManager();
+    ~SoundManager();
+
+    void AddSound();
+    void AddBGM();
+private:
+
+};
+
+
+
+#endif
